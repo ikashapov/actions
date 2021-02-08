07132e391f4342a3900b023f3546a85c618c1157
Ilya Kashapov <matem@matem.ru>
2021-02-08 04:50:42 +0300

    run build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# clean ENV from previous run
+#for i in $(env | awk -F"=" '{print $1}') ; do
+#unset $i ; done
+
+# set -x or set -o xtrace expands variables and prints a little + sign before the line.
+# set -v or set -o verbose does not expand the variables before printing.
+# Use set +x and set +v to turn off the above settings.
+set -x #echo on
+
+#Exit if a variable is not set
+set -o nounset
+
+#Exit on first error
+set -o errexit
+
+# cd to folder with bash script
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+ls -la
